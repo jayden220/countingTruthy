3939e3635b1d6a5f4d29ddaca7beef99076045f8
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-04 16:37:12 +0200

    first

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const list = [true,true,true,false,false,false,true]
+
+function count(){
+    for (item of list) {
+        if(item == true ){
+            console.log(item)
+        }else{
+            console.log(item)
+        }
+    }
+}
+
+count()
